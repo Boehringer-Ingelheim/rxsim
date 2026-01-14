7639c39dbc04fea1c1f64466a54144b7609a29cd
Saumil Shah <saumil.shah@boehringer-ingelheim.com>
2026-01-14 10:23:57 +0100

    language consistency

--- R/Trial.R
+++ R/Trial.R
@@ -1,8 +1,7 @@
-Trials <- R6::R6Class(
-  "Trials",
-
+Trial <- R6::R6Class(
+  classname = "Trial",
   public = list(
-    # Fields
+    # fields
     name = NULL,
     description = NULL,
     silent = FALSE,
@@ -12,7 +11,7 @@ Trials <- R6::R6Class(
     locked_data = NULL,
     results = NULL,
 
-    # Constructor
+    # constructor
     initialize = function(
     name,
     description = name,
@@ -38,8 +37,9 @@ Trials <- R6::R6Class(
       self$results <- results
     },
 
-    # Fit method: loop through timelist and apply reader conditions
-    fit = function() {
+    # methods
+    # run method: loop through timelist and apply conditions
+    run = function() {
       if (is.null(self$timer) || length(self$population) == 0) {
         stop("Timer and population list must be set before running fit()")
       }
@@ -49,7 +49,7 @@ Trials <- R6::R6Class(
       for (i in seq_len(n_timepoints)) {
         tp <- self$timer$get_timepoint(i)
 
-        # Apply enrollment/dropout to each Population object in the list
+        # apply enrollment/dropout to each Population object in the list
         for (p in self$population) {
           if (!is.null(tp$enroller)) {
             p$set_enrolled(tp$enroller, time = i)
@@ -59,45 +59,23 @@ Trials <- R6::R6Class(
           }
         }
 
-        # Collect snapshots from all populations
+        # collect snapshots from all populations
         locked_snapshot <- lapply(self$population, function(p) p$get_data())
         n_events <- sum(sapply(self$population, function(p) sum(!is.na(p$enrolled))))
 
-        # Run all reader conditions
-        reader_results <- self$timer$run_readers(
+        # check all conditions
+        results <- self$timer$check_conditions(
           locked_data = locked_snapshot,
           current_time = i,
           n_events = n_events
         )
 
-        if (length(reader_results) > 0) {
+        if (length(results) > 0) {
           self$locked_data[[paste0("time_", i)]] <- locked_snapshot
-          self$results[[paste0("time_", i)]] <- reader_results
+          self$results[[paste0("time_", i)]] <- results
         }
       }
     }
-  )
-)
 
-# Two populations
-pop1 <- Population$new("Arm A", data = rnorm(20, mean = 50))
-pop2 <- Population$new("Arm B", data = rnorm(20, mean = 55))
-
-# Timers with multiple reader conditions
-t <- Timers$new(name = "TrialTimers")
-t$add_timepoint(dropper = 2, enroller = 5)
-t$add_timepoint(dropper = 1, enroller = 3)
-
-# Add reader conditions
-t$add_reader_condition(time = 1, n_events = 5, func = function(ld,time) lapply(ld, mean))
-t$add_reader_condition(time = 2, threshold = 10)  # snapshot if >=10 events
-# Add in condition flag of TTE
-
-# Trial with list of populations
-trial <- Trials$new(name = "Trial A", timer = t, population = list(pop1, pop2))
-
-# Run
-trial$fit()
-
-# Inspect results
-trial$results
+  ) # end public
+) # end class
